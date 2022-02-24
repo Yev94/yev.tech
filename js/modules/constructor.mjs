9072d5c9d58416ea4322d78a9b45adf6023e718d
Yev <tjs_694@hotmail.com>
2022-02-24 17:28:30 +0100

    Added TODO comment

--- js/modules/constructor.mjs
+++ js/modules/constructor.mjs
@@ -1,9 +1,11 @@
 import { append, appendTitle } from './insert.mjs'
 
 export let constructor = (directory) => {
+    //TODO: Pasar la búqueda de elementos objetos del tipo html.getID('');
     let mainHTML = document.getElementsByTagName('main')[0];
     let titleHTML = document.getElementsByTagName('title')[0];
     let menuToggle = document.getElementById('drop');
+
 
     //Creamos una función que se auto-ejecuta para que sea asíncrona y espere a que termine de ejecutarse las funciones a las que llama
     ; (async () => {
