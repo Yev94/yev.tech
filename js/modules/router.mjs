80fb9a465eb5b219569194246044dbf2c2124f3f
Yev <tjs_694@hotmail.com>
2022-07-08 07:55:16 +0200

    Added new article

--- js/modules/router.mjs
+++ js/modules/router.mjs
@@ -4,6 +4,6 @@ import { pages } from './routes.mjs';
 // TODO: Pasar a un JSON el objeto cuando aprenda a usar la API rest para hacer las conexiones usando: var arrDatos = JSON.parse(element.responseText); Para convertir el JSON a un objeto
 
 export let router = () => {
-    let { hash } = window.location;
-    constructor(pages[hash]);
+  let { hash } = window.location;
+  constructor(pages[hash]);
 }
--- js/modules/routes.mjs
+++ js/modules/routes.mjs
@@ -1,109 +1,111 @@
 export let pages = {
-    
-    '#/': "./html/home.html",
-    '': "./html/home.html",
+  
+  '#/': "./html/home.html",
+  '': "./html/home.html",
 
-    '#/admin/solicitud-prestacion-desempleo': './html/admin/solicitud-prestacion-desempleo.html',
+  '#/admin/solicitud-prestacion-desempleo': './html/admin/solicitud-prestacion-desempleo.html',
 
-    '#/descripcion-codigos/bbdd-servidor/conexion-bbdd':
+  '#/descripcion-codigos/bbdd-servidor/conexion-bbdd':
     './html/descripcion-codigos/bbdd-servidor/conexion-bbdd.html',
 
-    
-    '#/descripcion-codigos/bbdd-servidor/enviar-mail':
+
+  '#/descripcion-codigos/bbdd-servidor/enviar-mail':
     './html/descripcion-codigos/bbdd-servidor/enviar-mail.html',
 
-    '#/descripcion-codigos/bbdd-servidor/estructura-archivos':
+  '#/descripcion-codigos/bbdd-servidor/estructura-archivos':
     './html/descripcion-codigos/bbdd-servidor/estructura-archivos.html',
-    
-    '#/descripcion-codigos/bbdd-servidor/evitar-inyeccion-sql':
+
+  '#/descripcion-codigos/bbdd-servidor/evitar-inyeccion-sql':
     './html/descripcion-codigos/bbdd-servidor/evitar-inyeccion-sql.html',
 
-    '#/descripcion-codigos/bbdd-servidor/htaccess':
+  '#/descripcion-codigos/bbdd-servidor/htaccess':
     './html/descripcion-codigos/bbdd-servidor/htaccess.html',
-    
-    '#/descripcion-codigos/bbdd-servidor/imagenes':
+
+  '#/descripcion-codigos/bbdd-servidor/imagenes':
     './html/descripcion-codigos/bbdd-servidor/imagenes.html',
-    
-    '#/descripcion-codigos/bbdd-servidor/mvc':
+
+  '#/descripcion-codigos/bbdd-servidor/mvc':
     './html/descripcion-codigos/bbdd-servidor/mvc.html',
-    
-    '#/descripcion-codigos/bbdd-servidor/sistema-sesiones':
+
+  '#/descripcion-codigos/bbdd-servidor/sistema-sesiones':
     './html/descripcion-codigos/bbdd-servidor/sistema-sesiones.html',
-    
-    '#/descripcion-codigos/cliente/conexion-get-post':
+
+  '#/descripcion-codigos/cliente/conexion-get-post':
     './html/descripcion-codigos/cliente/conexion-get-post.html',
-    
-    '#/descripcion-codigos/cliente/extensiones-chrome':
+
+  '#/descripcion-codigos/cliente/extensiones-chrome':
     './html/descripcion-codigos/cliente/extensiones-chrome.html',
 
-    '#/descripcion-codigos/cliente/intervalo-js':
+  '#/descripcion-codigos/cliente/intervalo-js':
     './html/descripcion-codigos/cliente/intervalo-js.html',
 
-    '#/descripcion-codigos/cliente/proxies-js':
+  '#/descripcion-codigos/cliente/proxies-js':
     './html/descripcion-codigos/cliente/proxies-js.html',
-    
-    '#/descripcion-codigos/encontrar-suma-num-array':
+
+  '#/descripcion-codigos/encontrar-suma-num-array':
     './html/descripcion-codigos/encontrar-suma-num-array.html',
-    
-    
-    '#/descripcion-codigos/encriptacion':
+
+
+  '#/descripcion-codigos/encriptacion':
     './html/descripcion-codigos/encriptacion.html',
 
-    
-    '#/descripcion-codigos/expresiones-regulares':
+
+  '#/descripcion-codigos/expresiones-regulares':
     './html/descripcion-codigos/expresiones-regulares.html',
-    
-    '#/descripcion-codigos/fibonnaci':
+
+  '#/descripcion-codigos/fibonnaci':
     './html/descripcion-codigos/fibonnaci.html',
-    
-    '#/descripcion-codigos/iteradores-iterables':
+
+  '#/descripcion-codigos/iteradores-iterables':
     './html/descripcion-codigos/iteradores-iterables.html',
-    
-    '#/descripcion-codigos/numeros-aleatorios-entre-rangos':
+
+  '#/descripcion-codigos/numeros-aleatorios-entre-rangos':
     './html/descripcion-codigos/numeros-aleatorios-entre-rangos.html',
-    
-    '#/descripcion-codigos/ordenar-array':
+
+  '#/descripcion-codigos/ordenar-array':
     './html/descripcion-codigos/ordenar-array-1D.html',
-    
-    '#/descripcion-codigos/programando-en-batch':
+
+  '#/descripcion-codigos/programando-en-batch':
     './html/descripcion-codigos/programando-en-batch.html',
-    
-    '#/descripcion-codigos/recursividad':
+
+  '#/descripcion-codigos/recursividad':
     './html/descripcion-codigos/recursividad.html',
-    
-    
-    '#/estudio-anki': './html/estudio-anki',
-    '#/frameworks': './html/frameworks',
-    '#/frameworks/laravel': './html/frameworks/laravel.html',
-    
-    '#/herramientas': './html/herramientas',
-    '#/herramientas/tracking/gtm': './html/herramientas/tracking/gtm',
-    '#/herramientas/tracking/gtm/cliente': './html/herramientas/tracking/gtm/cliente.html',
-    '#/herramientas/vs-code/configuracion': './html/herramientas/vs-code/configuracion.html',
-    '#/herramientas/vs-code/extensiones': './html/herramientas/vs-code/extensiones.html',
-    '#/herramientas/vs-code/snippets': './html/herramientas/vs-code/snippets.html',
-    '#/herramientas/vs-code/testing': './html/herramientas/vs-code/testing.html',
-    '#/herramientas/wamp': './html/herramientas/wamp',
-    '#/herramientas/wamp/phpmyadmin': './html/herramientas/wamp/phpmyadmin.html',
 
-    '#/os/linux/dns': './html/os/linux/dns.html',
-    '#/os/linux/mime': './html/os/linux/mime.html',
-    '#/os/linux/vsftp': './html/os/linux/vsftp.html',
 
-    '#/os/macos': './html/os/macos',
-    
+  '#/estudio-anki': './html/estudio-anki',
+  '#/frameworks': './html/frameworks',
+  '#/frameworks/laravel': './html/frameworks/laravel.html',
 
-    '#/cms/prestashop': './html/cms/prestashop',
-    '#/cms/prestashop/configuracion-avanzada': './html/cms/prestashop/configuracion-avanzada.html',
-    '#/cms/prestashop/instalar-prestashop-local': './html/cms/prestashop/instalar-prestashop-local.html',
-    '#/cms/prestashop/primeros-pasos-en-prestashop': './html/cms/prestashop/primeros-pasos-en-prestashop.html',
-    '#/cms/prestashop/cambios': './html/cms/prestashop/cambios.html',
-    '#/cms/prestashop/features': './html/cms/prestashop/features.html',
+  '#/herramientas': './html/herramientas',
+  '#/herramientas/tracking/gtm': './html/herramientas/tracking/gtm',
+  '#/herramientas/tracking/gtm/cliente': './html/herramientas/tracking/gtm/cliente.html',
+  '#/herramientas/tracking/gtm/cookies': './html/herramientas/tracking/gtm/cookies.html',
 
-    '#/cms/wordpress': './html/cms/wordpress',
-    '#/cms/wordpress/configuracion-avanzada': './html/cms/wordpress/configuracion-avanzada.html',
-    '#/cms/wordpress/instalar-wordpress-local': './html/cms/wordpress/instalar-wordpress-local.html',
-    '#/cms/wordpress/primeros-pasos-en-wordpress': './html/cms/wordpress/primeros-pasos-en-wordpress.html',
-    '#/cms/wordpress/temas/avada': './html/cms/wordpress/temas/avada.html',
-    '#/cms/wordpress/temas/divi': './html/cms/wordpress/temas/divi.html',
+  '#/herramientas/vs-code/configuracion': './html/herramientas/vs-code/configuracion.html',
+  '#/herramientas/vs-code/extensiones': './html/herramientas/vs-code/extensiones.html',
+  '#/herramientas/vs-code/snippets': './html/herramientas/vs-code/snippets.html',
+  '#/herramientas/vs-code/testing': './html/herramientas/vs-code/testing.html',
+  '#/herramientas/wamp': './html/herramientas/wamp',
+  '#/herramientas/wamp/phpmyadmin': './html/herramientas/wamp/phpmyadmin.html',
+
+  '#/os/linux/dns': './html/os/linux/dns.html',
+  '#/os/linux/mime': './html/os/linux/mime.html',
+  '#/os/linux/vsftp': './html/os/linux/vsftp.html',
+
+  '#/os/macos': './html/os/macos',
+
+
+  '#/cms/prestashop': './html/cms/prestashop',
+  '#/cms/prestashop/configuracion-avanzada': './html/cms/prestashop/configuracion-avanzada.html',
+  '#/cms/prestashop/instalar-prestashop-local': './html/cms/prestashop/instalar-prestashop-local.html',
+  '#/cms/prestashop/primeros-pasos-en-prestashop': './html/cms/prestashop/primeros-pasos-en-prestashop.html',
+  '#/cms/prestashop/cambios': './html/cms/prestashop/cambios.html',
+  '#/cms/prestashop/features': './html/cms/prestashop/features.html',
+
+  '#/cms/wordpress': './html/cms/wordpress',
+  '#/cms/wordpress/configuracion-avanzada': './html/cms/wordpress/configuracion-avanzada.html',
+  '#/cms/wordpress/instalar-wordpress-local': './html/cms/wordpress/instalar-wordpress-local.html',
+  '#/cms/wordpress/primeros-pasos-en-wordpress': './html/cms/wordpress/primeros-pasos-en-wordpress.html',
+  '#/cms/wordpress/temas/avada': './html/cms/wordpress/temas/avada.html',
+  '#/cms/wordpress/temas/divi': './html/cms/wordpress/temas/divi.html',
 }
