22fe63ea88701aa9336681137ab5e97b04f84adf
Yev Tyurin <eutj.694@gmail.com>
2022-09-21 20:20:00 +0200

    Fix Bug URL

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ import Calc from './modules/calc.mjs';
 document.addEventListener("DOMContentLoaded", () => router());
 window.addEventListener("hashchange", () => router());
 
-if (window.location.href == "http://127.0.0.1:5501/index.html#/calc"){
+if (window.location.href.includes('#/calc')){
   let calc = new Calc();
 }
