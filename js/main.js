299f5719aca9d81dbc7572e3a2cdf5c54d676c7b
Yev Tyurin <eutj.694@gmail.com>
2023-08-18 02:46:15 +0200

    Y otra vez todo

--- js/main.js
+++
@@ -1,6 +0,0 @@
-import {router} from "./modules/router.mjs";
-
-
-document.addEventListener("DOMContentLoaded", () => router());
-window.addEventListener("hashchange", () => router());
-
--- js/modules/router.mjs
+++
@@ -1,9 +0,0 @@
-import { constructor } from './constructor.mjs';
-import { pages } from './routes.mjs';
-
-// TODO: Pasar a un JSON el objeto cuando aprenda a usar la API rest para hacer las conexiones usando: var arrDatos = JSON.parse(element.responseText); Para convertir el JSON a un objeto
-
-export let router = () => {
-  let { hash } = window.location;
-  constructor(pages[hash]);
-}
